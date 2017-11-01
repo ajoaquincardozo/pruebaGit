7262c5deceb87211869a2b7f9b73c2ae6c382e06
ajoaquincardozo <a.joaquincardozo@gmail.com>
2017-11-01 17:32:51 -0300

    Se replico el ejemplo de ventana y boton sin robot

--- ejemploConGit/src/sample/Main.java
+++ ejemploConGit/src/sample/Main.java
@@ -1,18 +1,38 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.Group;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
 
 public class Main extends Application {
 
+    GraphicsContext gc;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Estoy probando que onda esto");
+        Group canvasContainer = new Group();
+        Canvas canvas= new Canvas(460,200);
+        gc = canvas.getGraphicsContext2D();
+        canvasContainer.getChildren().add(canvas);
+
+        Button botonNothing = new Button();
+        botonNothing.setText("Clickeame pero no haga nada");
+
+        HBox contenedorHorizontal = new HBox(botonNothing);
+        VBox contenedorPrincipal = new VBox(contenedorHorizontal,canvasContainer);
+
+        contenedorPrincipal.setSpacing(10);
+        contenedorPrincipal.setPadding(new Insets(20));
+
+        primaryStage.setScene(new Scene(contenedorPrincipal,500,300));
         primaryStage.show();
     }
 
